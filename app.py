f0e87478f503f9179118f7f443aa89afa52c9cb5
Breno Enedino Carrijo Pereira <brenoestudogeral@gmail.com>
2026-02-16 07:18:46 -0300

    Banco de Dados - Commit 2

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request, redirect
+from models import db, Usuario
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///intercambio.db' #configuração e diretório do db (SQLite)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #desliga funções desnecessárias do SQLAlchemy
+
+db.init_app(app) #conecta o db com o Flask (servidor)
+
+with app.app_context(): #abre a "conexão"/processo para db e Flask trabalharem juntos
+        db.create_all() #cria todas tabelas atreladas ao db (tabela(s) no models.py)
+        #o arquivo db será criado na instance
 
 @app.route("/")
 def home():
